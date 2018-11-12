70c1843a94604e98838c49a6d6ddce0d664f67d5
Mr.Piak <1029582431@qq.com>
2018-11-12 16:50:55 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,5 @@
 function js_confirm()
 {
-  var ret;
-  ret=confirm("Please Reportal");
-  if(ret=true)
-  {
-    window.location.href="https://www.baidu.com/";
-  }
+  document.write(Date());
 }
 
